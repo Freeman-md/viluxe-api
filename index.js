efdff5ba58ce363672f17f0fcebb7af935f7bf96
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-09-18 07:18:56 +0100

    UPDATE: setup endpoint to create payment intent

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SK_KEY);
+
+const app = express();
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret, amount });
+  } catch (error) {
+    console.error('Error creating Payment Intent:', error);
+    res.status(500).json({ error: 'Error creating Payment Intent' });
+  }
+});
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
